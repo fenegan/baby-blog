33dc99214abef3a79f487f635ad65e312c3cad84
Killian Vermersch <killian@idequanet.com>
2017-11-30 12:29:36 +0100

    Added comments to article page

--- article.php
+++ article.php
@@ -1,17 +1,54 @@
 <?php
 $id = $_GET['id'];
 $link = mysqli_connect('localhost', 'babyblog', 'toto42', 'babyblog');
+
+$message = '';
+if (isset($_POST['message']))
+{
+    $message = htmlentities($_POST['message']);
+    $q = "INSERT INTO `comments` (`id`, `post_id`, `message`) VALUES (NULL, '".$id."', '".$message."')";
+    mysqli_query($link, $q);
+    
+    header('Location: article.php?id='.$id);
+    exit();
+}
+
 $result = mysqli_query($link, 'SELECT * FROM posts WHERE id = '.$id);
 $article = mysqli_fetch_assoc($result);
+
+$result = mysqli_query($link, 'SELECT * FROM comments WHERE post_id = '.$id);
+$comments = [];
+while ($row = mysqli_fetch_assoc($result))
+{
+    $comments[] = $row;
+}
 
 $title = $article['title'];
 
 ob_start();
 ?>
-<article class="col">
+<div class="col">
+<article>
     <h2><?= $article['title'] ?></h2>
     <div><?= nl2br($article['content']) ?></div>
 </article>
+<hr>
+<h3>Comments</h3>
+<div id="comments">
+    <?php foreach ($comments as $comment): ?>
+    <div class="comment">
+        <p class="message"><?= $comment['message'] ?></p>
+    </div>
+    <?php endforeach; ?>
+</div>
+<form action="article.php?id=<?= $id ?>" method="POST">
+    <div class="form-group">
+        <label for="message"></label>
+        <textarea name="message" id="message" class="form-control"></textarea>
+    </div>
+    <input type="submit" value="Submit" class="btn btn-primary" />
+</form>
+</div>
 <?php
 $content = ob_get_contents();
 ob_end_clean();
--- babyblog.sql
+++ babyblog.sql
@@ -1,51 +1,53 @@
--- MySQL dump 10.13  Distrib 5.5.55, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: babyblog
--- ------------------------------------------------------
--- Server version	5.5.55-0+deb8u1
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `post_id` int(10) UNSIGNED NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `posts`
 --
 
-DROP TABLE IF EXISTS `posts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `posts` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL,
   `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  PRIMARY KEY (`id`)
+  `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `posts`
+-- Indexes for dumped tables
 --
 
-LOCK TABLES `posts` WRITE;
-/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
 
--- Dump completed on 2017-11-24 11:29:06
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
